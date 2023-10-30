4610095512c32665fdb6d73ec45623dd80bbb05c
senaKash <62939178+senaKash@users.noreply.github.com>
2023-10-30 14:22:17 +0300

    Добавьте файлы проекта.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IT_LETI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("get-ass")]
+        public async Task<IActionResult> Get()
+        {
+            _logger.LogInformation("Запупа стопапупа(сработал get-ass)");
+            return await Task.Run(Ok);
+        }
+
+        /*
+        [HttpPost("sasiPipku")]
+        public async Task<IActionResult> CreatePost([FromBody] CreatePostPisos model)
+        {
+
+        }
+        */
+    }
+}
---
+++ src/Calculator/Host/IT-LETI/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Calculator.Contracts.Test;
+
+namespace IT_LETI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("get-test")]
+        public async Task<IActionResult> Get()
+        {
+            _logger.LogInformation("Запрос опсос");
+            return await Task.Run(Ok);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTest([FromBody] CreateTestDto model)
+        {
+
+
+
+        }
+
+
+    }
+}
